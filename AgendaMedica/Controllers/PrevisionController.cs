f72901b3248332da0b075c32967494f982e39cd9
CristobalHMP <cristobal.munoz2@virginiogomez.cl>
2023-05-16 14:12:36 -0400

    c1

---
+++ AgendaMedica/Controllers/PrevisionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AgendaMedica.Models;
+
+
+namespace AgendaMedica.Controllers
+{
+    public class PrevisionController : Controller
+    {
+        //instancia la clase que contiene la conexión a la db
+        private BdagendaContext db = new();
+
+        //método para listar todas las marcas
+        public IActionResult Index()
+        {
+            //select * from prevision
+            return View(db.Previsions.ToList());
+        }
+
+        //método para crear
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
