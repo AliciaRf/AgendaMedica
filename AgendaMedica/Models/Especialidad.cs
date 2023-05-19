c11b0dd6d6633a17c025fa173f3574e97c3b82cf
Alicia <Alicia>
2023-05-19 18:14:49 -0400

    p7

--- AgendaMedica/Models/Especialidad.cs
+++ AgendaMedica/Models/Especialidad.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace AgendaMedica.Models;
 
 public partial class Especialidad
 {
     public int IdEsp { get; set; }
 
+    [Required, MinLength(3, ErrorMessage ="La {0} debe tener minimo {1} caracteres"),  MaxLength(50)]
     public string Especialidad1 { get; set; } = null!;
 
     public virtual ICollection<Agendar> Agendars { get; set; } = new List<Agendar>();
